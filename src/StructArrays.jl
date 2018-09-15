2c40ee29a39dd16b14f320c5bfabeb66e5079ef1
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-09-15 19:23:58 +0100

    update requires

--- src/StructArrays.jl
+++ src/StructArrays.jl
@@ -1,10 +1,11 @@
 module StructArrays
 
-import Tables
+import Requires
 export StructArray
 
 include("structarray.jl")
 include("utils.jl")
-include("tables.jl")
+
+Requires.@require Tables="bd369af6-aec1-5ad0-b16a-f7cc5008161c" include("tables.jl")
 
 end # module
