269d28d4ccffa95f19585f30eee96809f3ebb3a4
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-09-15 17:45:38 +0100

    added kwarg constructor

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,6 +22,13 @@ end
     @test t3 == t
 end
 
+@testset "kwargs constructor" begin
+    a = [1.2]
+    b = [2.3]
+    @test StructArray(a=a, b=b) == StructArray((a=a, b=b))
+    @test StructArray{ComplexF64}(re=a, im=b) == StructArray{ComplexF64}(a, b)
+end
+
 @testset "complex" begin
     a, b = [1 2; 3 4], [4 5; 6 7]
     t = StructArray{ComplexF64}(a, b)
