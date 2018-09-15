dad5bd833ade69083adceb4c58febccf32f9b0c5
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-09-15 19:35:22 +0100

    fix requires

--- src/StructArrays.jl
+++ src/StructArrays.jl
@@ -6,6 +6,8 @@ export StructArray
 include("structarray.jl")
 include("utils.jl")
 
-Requires.@require Tables="bd369af6-aec1-5ad0-b16a-f7cc5008161c" include("tables.jl")
+function __init__()
+    Requires.@require Tables="bd369af6-aec1-5ad0-b16a-f7cc5008161c" include("tables.jl")
+end
 
 end # module
