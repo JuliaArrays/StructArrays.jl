3209429ae1e0de7223b83e239a7d8937c9387c65
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-02-20 18:30:46 +0000

    added pool

--- src/StructArrays.jl
+++ src/StructArrays.jl
@@ -15,6 +15,9 @@ function __init__()
     Requires.@require Tables="bd369af6-aec1-5ad0-b16a-f7cc5008161c" include("tables.jl")
     Requires.@require PooledArrays="2dfb63ee-cc39-5dd5-95bd-886bf059d720" begin
         fastpermute!(v::PooledArrays.PooledArray, p::AbstractVector) = permute!(v, p)
+        function pool(v::AbstractArray{T}; condition) where T
+            condition(T) ? PooledArrays.PooledArray(v) : v
+        end
     end
     Requires.@require WeakRefStrings="ea10d353-3f73-51f8-a26c-33c1cb351aa5" begin
         fastpermute!(v::WeakRefStrings.StringArray, p::AbstractVector) = permute!(v, p)
