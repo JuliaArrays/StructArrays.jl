8a946a86179bb80aaeb2abce75f96241056049eb
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-26 18:46:12 +0000

    more docs

--- src/collect.jl
+++ src/collect.jl
@@ -31,8 +31,7 @@ _reshape(v, itr, ::Union{Base.HasLength, Base.SizeUnknown}) = v
 Collects `itr` into a `StructArray`. The user can optionally pass a `initializer`, that is to say
 a function `(S, d) -> v` that associates to a type and a size an array of eltype `S`
 and size `d`. By default `initializer` returns a `StructArray` of `Array` but custom array types
-may be used.
-
+may be used. `fr` represents the moment in the iteration of `itr` from which to start collecting.
 """
 collect_structarray(itr; initializer = default_initializer) =
     collect_structarray(itr, iterate(itr); initializer = initializer)
