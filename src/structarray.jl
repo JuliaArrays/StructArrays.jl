efb75eb1e81f7ed01c5ac1bb332d9125803b2ba4
Lucas C Wilcox <lucas@swirlee.com>
2019-09-18 09:48:58 -0700

    Avoid dynamic code in get_ith
    This uses a generated function to avoid a dynamic call for `getindex`
    allowing it to be called in a CUDAnative kernel.

--- src/structarray.jl
+++ src/structarray.jl
@@ -134,11 +134,11 @@ Base.axes(s::StructArray) = axes(fieldarrays(s)[1])
 Base.axes(s::StructArray{<:Any, <:Any, <:EmptyTup}) = (1:0,)
 
 get_ith(cols::NamedTuple, I...) = get_ith(Tuple(cols), I...)
-function get_ith(cols::NTuple{N, Any}, I...) where N
-    ntuple(N) do i
-        @inbounds res = getfield(cols, i)[I...]
-        return res
+@generated function get_ith(cols::NTuple{N, Any}, I...) where N
+    args = ntuple(N) do i
+        :(@inbounds res = getfield(cols, $i)[I...])
     end
+    :(($(args...),))
 end
 
 Base.@propagate_inbounds function Base.getindex(x::StructArray{T, <:Any, <:Any, CartesianIndex{N}}, I::Vararg{Int, N}) where {T, N}
