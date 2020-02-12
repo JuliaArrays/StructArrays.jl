ae6a52bf231a65038c484310064c062284c9d692
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-11 23:40:27 -0800

    Implement append!(::StructVector, ::Any) using Tables.isrowtable

--- src/tables.jl
+++ src/tables.jl
@@ -6,3 +6,19 @@ Tables.columnaccess(::Type{<:StructVector}) = true
 Tables.columns(s::StructVector) = fieldarrays(s)
 
 Tables.schema(s::StructVector) = Tables.Schema(staticschema(eltype(s)))
+
+function Base.append!(s::StructVector, rows)
+    if Tables.isrowtable(rows) && Tables.columnaccess(rows)
+        # Input `rows` is a container of rows _and_ satisfies column
+        # table interface.  Thus, we can add the input column-by-column.
+        table = Tables.columns(rows)
+        nt = foldl(Tables.columnnames(table); init = NamedTuple()) do nt, name
+            (; nt..., name => Tables.getcolumn(table, name))
+        end
+        return append!(s, StructArray(nt))
+    else
+        # Otherwise, fallback to a generic implementation expecting
+        # that `rows` is an iterator:
+        return foldl(push!, rows; init=s)
+    end
+end
