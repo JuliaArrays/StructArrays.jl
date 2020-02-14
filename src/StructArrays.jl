ab45e83740dce99b7f44e09512e6d7be88d65ee7
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2020-02-14 17:53:22 +0000

    move DataAPI compatibility in main file

--- src/StructArrays.jl
+++ src/StructArrays.jl
@@ -10,12 +10,20 @@ export replace_storage
 include("interface.jl")
 include("structarray.jl")
 include("utils.jl")
-include("refarray.jl")
 include("collect.jl")
 include("sort.jl")
 include("groupjoin.jl")
 include("lazy.jl")
 include("tables.jl")
+
+# Implement refarray and refvalue to deal with pooled arrays and weakrefstrings effectively
+import DataAPI: refarray, refvalue
+
+refarray(s::StructArray) = StructArray(map(refarray, fieldarrays(s)))
+
+function refvalue(s::StructArray{T}, v::Tup) where {T}
+    createinstance(T, map(refvalue, fieldarrays(s), v)...)
+end
 
 # Use Adapt allows for automatic conversion of CPU to GPU StructArrays
 import Adapt
--- src/refarray.jl
+++
@@ -1,7 +0,0 @@
-import DataAPI: refarray, refvalue
-
-refarray(s::StructArray) = StructArray(map(refarray, fieldarrays(s)))
-
-function refvalue(s::StructArray{T}, v::Tup) where {T}
-    createinstance(T, map(refvalue, fieldarrays(s), v)...)
-end
