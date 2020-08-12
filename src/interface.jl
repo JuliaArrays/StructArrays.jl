b249c1f8feec03472bc1aba5c558fcd724cddca0
piever <pietro.vertechi@protonmail.com>
2020-08-12 11:47:50 +0100

    minor reorg

--- src/interface.jl
+++ src/interface.jl
@@ -9,16 +9,8 @@ end
 
 staticschema(::Type{T}) where {T<:Tup} = T
 
-@generated function bypass_constructor(::Type{T}, args) where {T}
-    if isconcretetype(T)
-        vars = ntuple(_ -> gensym(), fieldcount(T))
-        assign = [:($var::$(fieldtype(T, i)) = getfield(args, $i)) for (i, var) in enumerate(vars)]
-        construct = Expr(:new, :T, vars...)
-        Expr(:block, assign..., construct)
-    else
-        Expr(:call, :T, Expr(:(...), :args))
-    end
+function createinstance(::Type{T}, args...) where {T}
+    isconcretetype(T) ? bypass_constructor(T, args) : T(args...)
 end
 
-createinstance(::Type{T}, args...) where {T} = bypass_constructor(T, args)
 createinstance(::Type{T}, args...) where {T<:Tup} = T(args)
