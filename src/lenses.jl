e87197c7d8176d32b5f7f3d59708e60b410c96b3
Chad Scherrer <chad.scherrer@gmail.com>
2020-12-04 09:02:26 -0800

    lenses.jl

---
+++ src/lenses.jl
@@ -0,0 +1,25 @@
+using BangBang
+using Setfield
+
+lenses(nt::NamedTuple) = _lenses(nt, ())
+
+function _lenses(nt::NamedTuple, acc)
+    result = ()
+    for k in keys(nt)
+        nt_k = getproperty(nt, k)
+        # Add "breadcrumb" steps to the accumulator as we descend into the tree
+        acc_k = push!!(acc, Setfield.PropertyLens{k}())
+        ℓ = _lenses(nt_k, acc_k)
+        result = append!!(result, ℓ)
+    end
+    return result
+end
+
+# When we reach a leaf node (an array), compose the steps to get a lens
+function _lenses(a::AbstractArray, acc)
+    return (Setfield.compose(acc...),)
+end
+
+nt = (a=(b=[1,2],c=(d=[3,4],e=[5,6])),f=[7,8]);
+
+lenses(nt)
