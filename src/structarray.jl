164202dca485622a013c43b155a693bb1402d3a9
Simon Byrne <simonbyrne@gmail.com>
2020-12-22 13:14:13 -0800

    add recursive example to docs

--- src/structarray.jl
+++ src/structarray.jl
@@ -110,10 +110,12 @@ StructVector{T}(args...; kwargs...) where {T} = StructArray{T}(args...; kwargs..
 StructVector(args...; kwargs...) = StructArray(args...; kwargs...)
 
 """
-    StructArray{T}(A::AbstractArray; dims)
+    StructArray{T}(A::AbstractArray; dims, unwrap=FT->FT!=eltype(A))
 
-Construct a `StructArray` from slices of `A` along `dims`. `T` will be
-recursively decomposed to fields of `eltype(A)`.
+Construct a `StructArray` from slices of `A` along `dims`. 
+
+The `unwrap` keyword argument is a function that determines whether to
+recursively convert fields of type `FT` to `StructArray`s.
 
 !!! compat "Julia 1.1"
      This function requires at least Julia 1.1.
@@ -134,19 +136,27 @@ julia> StructArray{Complex{Float64}}(X; dims=2)
  1.0 + 2.0im
  3.0 + 4.0im
 ```
+
+By default, fields will be unwrapped until they match the element type of the array:
+```
+julia> StructArray{Tuple{Float64,Complex{Float64}}}(rand(3,2); dims=1)
+2-element StructArray(view(::Array{Float64,2}, 1, :), StructArray(view(::Array{Float64,2}, 2, :), view(::Array{Float64,2}, 3, :))) with eltype Tuple{Float64,Complex{Float64}}:
+ (0.004767505234193781, 0.27949621887414566 + 0.9039320635041561im)
+ (0.41853472213051335, 0.5760165160827859 + 0.9782723869433818im)
+```
 """
-StructArray(A::AbstractArray; dims)
-function StructArray{T}(A::AbstractArray; dims) where {T}
+StructArray(A::AbstractArray; dims, unwrap)
+function StructArray{T}(A::AbstractArray; dims, unwrap=FT->FT!=eltype(A)) where {T}
     slices = Iterators.Stateful(eachslice(A; dims=dims))
-    buildfromslices(T, eltype(A), slices)
+    buildfromslices(T, unwrap, slices)
 end
-function buildfromslices(::Type{T}, ::Type{AT}, slices) where {T,AT}
-    if T == AT
-        return popfirst!(slices)
-    else
+function buildfromslices(::Type{T}, unwrap::F, slices) where {T,F}
+    if unwrap(T)
         buildfromschema(T) do FT
-            buildfromslices(FT, AT, slices)
+            buildfromslices(FT, unwrap, slices)
         end
+    else
+        return popfirst!(slices)
     end
 end
 
