582f6f26f85b1416e165b901485a7c9b687fc610
Jesse Chan <jesse.chan@rice.edu>
2021-04-20 15:30:53 -0500

    adding docs for StaticArray staticschema

--- src/staticarrays_support.jl
+++ src/staticarrays_support.jl
@@ -1,5 +1,10 @@
 import StaticArrays: StaticArray, tuple_prod
 
+"""
+    StructArrays.staticschema(::Type{<:StaticArray{S, T}}) where {S, T}
+
+The staticschema for a StaticArray is the underlying Tuple used to store the flattened array.
+"""
 @generated function StructArrays.staticschema(::Type{<:StaticArray{S, T}}) where {S, T}
     return quote
         Base.@_inline_meta
