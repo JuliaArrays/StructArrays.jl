b1a26c6da8c86a2a53e59e1191fe2b073795efa7
Jesse Chan <jesse.chan@rice.edu>
2021-09-08 16:21:11 -0500

    restricting StaticArrays schema to SArray instead of <:StaticArray

--- src/staticarrays_support.jl
+++ src/staticarrays_support.jl
@@ -1,19 +1,19 @@
-import StaticArrays: StaticArray, tuple_prod
+import StaticArrays: SArray, tuple_prod
 
 """
-    StructArrays.staticschema(::Type{<:StaticArray{S, T}}) where {S, T}
+    StructArrays.staticschema(::Type{<:SArray{S, T}}) where {S, T}
 
-The `staticschema` of a `StaticArray` element type is the `staticschema` of the underlying `Tuple`.
+The `staticschema` of an `SArray` element type is the `staticschema` of the underlying `Tuple`.
 ```julia
 julia> StructArrays.staticschema(SVector{2, Float64})
 Tuple{Float64, Float64}
 ```
 """
-@generated function StructArrays.staticschema(::Type{<:StaticArray{S, T}}) where {S, T}
+@generated function StructArrays.staticschema(::Type{<:SArray{S, T}}) where {S, T}
     return quote
         Base.@_inline_meta
         return NTuple{$(tuple_prod(S)), T}
     end
 end
-StructArrays.createinstance(::Type{T}, args...) where {T<:StaticArray} = T(args)
-StructArrays.component(s::StaticArray, i) = getindex(s, i)
+StructArrays.createinstance(::Type{T}, args...) where {T<:SArray} = T(args)
+StructArrays.component(s::SArray, i) = getindex(s, i)
