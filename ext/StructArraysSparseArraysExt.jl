8c772b7f8198d2cee5815de54c4c65d90b103b1a
piever <pietro.vertechi@protonmail.com>
2023-12-06 16:29:23 +0100

    use createinstance

--- ext/StructArraysSparseArraysExt.jl
+++ ext/StructArraysSparseArraysExt.jl
@@ -1,11 +1,11 @@
 module StructArraysSparseArraysExt
 
-using StructArrays: StructArray, components
+using StructArrays: StructArray, components, createinstance
 import SparseArrays: sparse, issparse
 
 function sparse(S::StructArray{T}) where {T}
     sparse_components = map(sparse, components(S))
-    T.(sparse_components...)
+    return createinstance.(T, sparse_components...)
 end
 
 issparse(S::StructArray) = all(issparse, components(S))
