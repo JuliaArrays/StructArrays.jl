0b8698ecdf5a758294b72e48effab0b4eb508ebf
José E. Cruz Serrallés <Jose.CruzSerralles@nyulangone.org>
2024-02-15 14:22:15 -0500

    Added keepat! for StructVector
    Added method Base.keepat! for StructVector. The code is nearly identical to that of Base.deleteat!.

--- src/structarray.jl
+++ src/structarray.jl
@@ -418,6 +418,11 @@ function Base.deleteat!(s::StructVector{T}, idxs) where T
     return StructVector{T}(t)
 end
 
+function Base.keepat!(s::StructVector{T}, idxs) where T
+    t = map(Base.Fix2(keepat!, idxs), components(s))
+    return StructVector{T}(t)
+end
+
 Base.copyto!(I::StructArray, J::StructArray) = (foreachfield(copyto!, I, J); I)
 
 function Base.copyto!(I::StructArray, doffs::Integer, J::StructArray, soffs::Integer, n::Integer)
