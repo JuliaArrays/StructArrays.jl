e744e14529f017be424ef2e0bbc4042b41f32c2b
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-03-14 20:29:04 +0530

    Fix import and move Aqua tests to the bottom

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,10 +12,7 @@ using LinearAlgebra
 using Test
 using SparseArrays
 using InfiniteArrays
-
-@testset "project quality" begin
-    Aqua.test_all(StructArrays, ambiguities=(; broken=true))
-end
+import Aqua
 
 using Documenter: doctest
 if Base.VERSION == v"1.6" && Int === Int64
@@ -1543,4 +1540,8 @@ end
     @test Base.IteratorSize(S) == Base.HasShape{2}()
     S = StructArray{Complex{Int}}((1:∞, 1:∞))
     @test Base.IteratorSize(S) == Base.IsInfinite()
+end
+
+@testset "project quality" begin
+    Aqua.test_all(StructArrays, ambiguities=(; broken=true))
 end
